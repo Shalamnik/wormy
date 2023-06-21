8bd1dc6786f0fd40ec4aefcc1fd6bcb7ac4a5a51
Шаламберидзе Никита Игоревич <nishalam@mts.ru>
2023-06-21 11:38:26 +0300

    fix: доработка отображения счёта в конце игры

--- game.py
+++ game.py
@@ -55,9 +55,8 @@ class Game:
         self.user_name = self.show_user_input()
         while True:
             self.run_game()
-            self.show_game_over_screen()
             self.db_worker.record_user_score(self.user_name, self.user_score)
-            self.show_result()
+            self.show_game_over_screen()
 
     def run_game(self):
         direction = choice([UP, DOWN, LEFT, RIGHT])
@@ -148,59 +147,30 @@ class Game:
         is_input_filled = False
         user_text = ''
 
-        # create rectangle
         input_rect = pygame.Rect(200, 200, 200, 60)
-
-        # active when input box is clicked by user
-        color_active = pygame.Color('lightskyblue3')
-
-        # color of input box
-        color_passive = pygame.Color('chartreuse4')
-        active = False
 
         while True:
             if is_input_filled:
-                return user_text
+                return user_text.rstrip()
 
             for event in pygame.event.get():
-
-                # if user types QUIT then the screen will close
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if input_rect.collidepoint(event.pos):
-                        active = True
-                    else:
-                        active = False
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN:
                         is_input_filled = True
 
-                    # Check for backspace
                     if event.key == pygame.K_BACKSPACE:
-
-                        # get text input from 0 to -1 i.e. end.
                         user_text = user_text[:-1]
-
-                    # Unicode standard is used for string
-                    # formation
                     else:
                         user_text += event.unicode
 
-            if active:
-                color = color_active
-            else:
-                color = color_passive
+            pygame.draw.rect(self.DISPLAY_SURF, DARK_GREEN, input_rect)
 
-                # draw rectangle and argument passed which should
-                # be on screen
-            pygame.draw.rect(self.DISPLAY_SURF, color, input_rect)
-
-            text_surface = self.BASIC_FONT.render(user_text, True, (255, 255, 255))
-            text_hint = self.BASIC_FONT.render('Input your name:', True, (255, 255, 255))
+            text_surface = self.BASIC_FONT.render(user_text, True, WHITE)
+            text_hint = self.BASIC_FONT.render('Input your name:', True, WHITE)
 
             # render at position stated in arguments
             self.DISPLAY_SURF.blit(text_surface, (input_rect.x + 5, input_rect.y + 35))
@@ -213,9 +183,6 @@ class Game:
             # display.flip() will update only a portion of the
             # screen to updated, not full area
             pygame.display.flip()
-
-            # clock.tick(60) means that for every second at most
-            # 60 frames should be passed.
             self.FPS_CLOCK.tick(60)
 
     @staticmethod
@@ -224,7 +191,9 @@ class Game:
         sys.exit()
 
     def show_game_over_screen(self):
-        game_over_font = pygame.font.Font('freesansbold.ttf', 150)
+        self.DISPLAY_SURF.fill(BG_COLOR)
+
+        game_over_font = pygame.font.Font('freesansbold.ttf', 60)
         game_surf = game_over_font.render('Game', True, WHITE)
         over_surf = game_over_font.render('Over', True, WHITE)
         game_rect = game_surf.get_rect()
@@ -234,6 +203,8 @@ class Game:
 
         self.DISPLAY_SURF.blit(game_surf, game_rect)
         self.DISPLAY_SURF.blit(over_surf, over_rect)
+
+        self.show_user_scores()
 
         self.draw.draw_press_key_msg(WINDOW_WIDTH, WINDOW_HEIGHT)
         pygame.display.update()
@@ -246,22 +217,31 @@ class Game:
                 pygame.event.get()
                 return
 
-    def show_result(self):
-        top_user_scores = self.db_worker.get_top_users()
+    def show_user_scores(self):
 
-        score_surf = self.BASIC_FONT.render(
-            f'{self.user_name}, your score: {self.user_score}',
+        user_score = f'{self.user_name}, your score: {self.user_score}'
+        top_header = f'Top 5 users:'
+        top_user_scores = self.db_worker.get_top_users()
+        # top_user_scores = [{'name': '1', 'score': 1}, {'name': '2', 'score': 2},
+        #                    {'name': '3', 'score': 3}, {'name': '4', 'score': 4},
+        #                    {'name': '5', 'score': 5}]
+
+        self.render_center_text(user_score)
+        self.render_center_text(top_header, 40)
+
+        margin_top = 60
+        for user in top_user_scores:
+            self.render_center_text(f"{user['name']}: {user['score']}", margin_top)
+            margin_top += 20
+
+    def render_center_text(self, text: str, margin_top: int = 0):
+        score_font = pygame.font.Font('freesansbold.ttf', 18)
+        score_surf = score_font.render(
+            text,
             True,
             WHITE
         )
         score_rect = score_surf.get_rect()
-        score_rect.midtop = (WINDOW_WIDTH - 20, 10)
+        score_rect.midtop = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + margin_top)
+
         self.DISPLAY_SURF.blit(score_surf, score_rect)
-        pygame.display.update()
-
-        self.is_key_pressed()
-
-        while True:
-            if self.is_key_pressed():
-                pygame.event.get()
-                return
